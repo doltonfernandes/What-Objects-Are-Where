6442d90946a3ac3a9c2d7b6301da6ca2d10c79b1
doltonfernandes <dolton.fernandes@gmail.com>
2021-03-27 23:59:55 +0530

    Added feature extraction

--- src/sel.py
+++ src/sel.py
@@ -1,5 +1,6 @@
-import cv2
-
+import cv2, torch
+from torchvision import models, transforms
+from PIL import Image
 
 def selectiveSearch(im):
     """
@@ -13,6 +14,28 @@ def selectiveSearch(im):
     rects = ss.process()
     return rects
 
+def featureExtractor(im):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(227),
+        transforms.ToTensor(),
+        transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )])
+    im = transform(im)
+    im = im.unsqueeze(0)
+    alexnet = models.alexnet(pretrained=True)
+    alexnet.eval()
+    return alexnet(im)
+
+def add_margin(pil_img, top, right, bottom, left, color):
+    width, height = pil_img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(pil_img.mode, (new_width, new_height), color)
+    result.paste(pil_img, (left, top))
+    return result
 
 if __name__ == "__main__":
     f = "../img/1.jpg"
@@ -22,4 +45,14 @@ if __name__ == "__main__":
     for rect in rects:
         x, y, w, h = rect
         cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 1, cv2.LINE_AA)
+
     cv2.imwrite("../img/1_sel.jpg", im)
+
+    # Get image in bounding box
+    im = Image.open(f)
+    im = im.crop((50, 10, 450, 400))
+    im.save('../img/1_bounding.jpg')
+
+    # Add p=16 pixels on border
+    im = add_margin(im, 16, 16, 16, 16, (0, 0, 0))
+    im = featureExtractor(im)
