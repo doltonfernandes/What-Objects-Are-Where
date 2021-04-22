e262ab759a9c579cac091252da71a921ce87965c
naren.akash <naren.akash@research.iiit.ac.in>
2021-04-22 15:56:01 +0530

    svm training code added [wip]

--- src/svmTraining.py
+++ src/svmTraining.py
@@ -17,4 +17,23 @@ optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 model.train()
 
+for epoch in range(epochs):
+    sum_loss = 0
 
+    for i in range(0, n, batch_size):
+        x = X
+        y = Y
+
+        x = Variable(x)
+        y = Variable(y)
+
+        optimizer.zero_grad()
+        output = model(x)
+
+        loss = torch.mean(torch.clamp(1 - output * y, min = 0))
+        loss.backward()
+        optimizer.step()
+
+        sum_loss += loss[0].data.cpu().numpy()
+
+    print("Epoch: {}, Loss: {}".format(epoch, sum_loss[0]))
