f2765bd9b2a3c5b373a328d0a687c11e79a6a39a
narenakash <rjnarenakash@hotmail.com>
2021-04-24 20:39:43 +0530

    svm model input shape updated

--- src/svmModel.py
+++ src/svmModel.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class SVM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fully_connected = nn.Linear(2, 1)
+        self.fully_connected = nn.Linear(1000, 1)
 
     def forward(self, x):
         output = self.fully_connected(x)
