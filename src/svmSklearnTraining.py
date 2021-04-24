d3025396d41a6711e2484eb897f3fd6555e3fa0f
Naren Akash RJ <rjnarenakash@hotmail.com>
2021-04-24 22:24:51 +0530

    svm training code updated

--- src/svmSklearnTraining.py
+++ src/svmSklearnTraining.py
@@ -1,6 +1,7 @@
 import torch
 
 import re
+import pickle
 from PIL import Image
 from os import listdir
 
@@ -11,17 +12,35 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-def trainClassifier(X, Y, val_split=0.20, num_classes=20):
+def trainClassifier(X, Y, val_split=0.80, num_classes=20):
     train_x, train_y = [], []
     val_x, val_y = [], []
 
     train_x, val_x, train_y, val_y = train_test_split(X, Y, stratify=Y, test_size=val_split)
 
-    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+    clf = make_pipeline(StandardScaler(), SVC(gamma='auto', verbose=True))
     clf.fit(train_x, train_y)
     
-    pred =clf.predict(val_x)
-    print(pred == val_y)
+    filename = 'svmmodel_pascalvoc.sav'
+    pickle.dump(clf, open(filename, 'wb'))
+
+    pred = clf.predict(train_x)
+    
+    correct = 0
+    for i in range(len(train_y)):
+        if pred[i] == train_y[i]:
+            correct += 1
+    print("Train classification accuracy: {}".format(correct / len(train_y)))
+
+    
+    pred = clf.predict(val_x)
+    
+    correct = 0
+    for i in range(len(val_y)):
+        if pred[i] == val_y[i]:
+            correct += 1
+    print(correct / len(val_y))
+    print("Validation classification accuracy: {}".format(correct / len(val_y)))
 
 if __name__ == "__main__":
     train_path = '../../features/'
